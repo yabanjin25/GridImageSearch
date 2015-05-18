d787714d42d6c4e65c8cf2d26aa0cfcdea3f4713
Alisa Yamanaka <alisa@box.com>
2015-05-17 18:59:31 -0700

    Enable user to share images

--- app/src/main/java/com/example/ayamanaka/gridimagesearch/activities/ImageDisplayActivity.java
+++ app/src/main/java/com/example/ayamanaka/gridimagesearch/activities/ImageDisplayActivity.java
@@ -1,17 +1,30 @@
 package com.example.ayamanaka.gridimagesearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.example.ayamanaka.gridimagesearch.R;
 import com.example.ayamanaka.gridimagesearch.TouchImageView;
 import com.example.ayamanaka.gridimagesearch.models.ImageResult;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ImageDisplayActivity extends ActionBarActivity {
+
+    private TouchImageView ivImageResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +35,7 @@ public class ImageDisplayActivity extends ActionBarActivity {
         // Pull out url from intent
         String url = imageResult.getFullUrl();
         // Find the image view
-        TouchImageView ivImageResult = (TouchImageView) findViewById(R.id.ivImageResult);
+        ivImageResult = (TouchImageView) findViewById(R.id.ivImageResult);
         Picasso.with(this).load(url).into(ivImageResult);
 
         ActionBar actionBar = getSupportActionBar();
@@ -46,6 +59,7 @@ public class ImageDisplayActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_share) {
+            onShareItem();
             return true;
         }
 
@@ -55,5 +69,49 @@ public class ImageDisplayActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Can be triggered by a view event such as a button press
+    public void onShareItem() {
+        // Get access to the URI for the bitmap
+        Uri bmpUri = getLocalBitmapUri(ivImageResult);
+        if (bmpUri != null) {
+            // Construct a ShareIntent with link to image
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+            shareIntent.setType("image/*");
+            // Launch sharing dialog for image
+            startActivity(Intent.createChooser(shareIntent, "Share Image"));
+        } else {
+            // ...sharing failed, handle error
+        }
+    }
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 }
